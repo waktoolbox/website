1003f8dbcec2ac96cc3d09b65f9651030c6ab5d1
Maude Clonet <clonet.maude@gmail.com>
2022-09-26 14:22:28 +0200

    fix (oauth): use env for redirection url in React

--- client/src/components/oauth/discord.tsx
+++ client/src/components/oauth/discord.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 function DiscordOAuth() {
+    console.log(process.env);
+
     return (
         <div>
-            <a href="https://discord.com/api/oauth2/authorize?client_id=1022461229958168609&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Fapi%2Foauth%2Fdiscord%2Fredirect&response_type=code&scope=email">
-                Auth with Discord
+
+            <a href={process.env.REACT_APP_DISCORD_OAUTH_URL}>
+            Auth with Discord
             </a>
         </div>
     );
