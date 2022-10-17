03fbd7382f5710ee09b3cda3abb254a927de0295
Maude Clonet <clonet.maude@gmail.com>
2022-10-17 20:55:04 +0200

    chore (wakfu warriors): balance pools while rolling it

--- server/chore/tournament/impl/wakfu-warriors-phase-controller.ts
+++ server/chore/tournament/impl/wakfu-warriors-phase-controller.ts
@@ -160,14 +160,17 @@ export class WakfuWarriorPhaseOne implements TournamentPhaseController<WakfuWarr
 
         if (!poolNumber || !poolSize) throw new Error("Bad configuration : missing pool information");
 
+        const computedStepToBalanceLoad = Math.floor(this.data.teams.length / poolNumber);
+
         const teams = [...this.data.teams].sort(() => Math.random() - 0.5)
 
         for (let i = 0; i < poolNumber; i++) {
             this.data.teamPool.push({
-                teams: [...teams.slice(i * poolSize, i * poolSize + poolSize).map(t => t.id || "")],
+                teams: [...teams.slice(i * computedStepToBalanceLoad, i * computedStepToBalanceLoad + computedStepToBalanceLoad).map(t => t.id || "")],
                 matches: []
             })
         }
+        this.data.teamPool[poolNumber - 1].teams.push(...teams.slice(poolNumber * computedStepToBalanceLoad, teams.length).map(t => t.id || ""));
 
         this.data.currentRound = 1;
         return this.prepareRound();
